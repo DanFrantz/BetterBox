884c7cc8c58b94bda2f005572308d297a2dd8cde
Daniel Paim Frantz <danielpaimf@edu.pucrs.br>
2024-09-25 14:58:28 -0300

    Sei lá

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +12,47 @@ app.get('/api/saudacao', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+*/
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.post.update({
+      where: {
+        slug: 'my-first-post',
+      },
+      data: {
+        comments: {
+          createMany: {
+            data: [
+              { comment: 'Great post!' },
+              { comment: "Can't wait to read more!" },
+            ],
+          },
+        },
+      },
+    })
+    const posts = await prisma.post.findMany({
+      include: {
+        comments: true,
+      },
+    })
+  
+    console.dir(posts, { depth: Infinity })
+  }
+  
+  
+main()
+  .catch(async (e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+
 
 
 
