a7c6eeda0bd065eea07117f7634b136517a92f8c
Daniel Paim Frantz <danielpaimf@edu.pucrs.br>
2024-09-30 16:01:23 -0300

    Se pá o schema deu bom

--- src/index.ts
+++ src/index.ts
@@ -18,28 +18,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.post.update({
-      where: {
-        slug: 'my-first-post',
-      },
-      data: {
-        comments: {
-          createMany: {
-            data: [
-              { comment: 'Great post!' },
-              { comment: "Can't wait to read more!" },
-            ],
-          },
-        },
-      },
-    })
-    const posts = await prisma.post.findMany({
-      include: {
-        comments: true,
-      },
-    })
-  
-    console.dir(posts, { depth: Infinity })
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
   }
   
   
